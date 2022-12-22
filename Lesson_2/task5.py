eed66cf5ed58b21d907683bac0ba3e755bf73d51
Makarenko <makar44m@gmail.com>
2022-12-22 23:47:26 +0200

    Searching of the max, min, sum of the number in the list

---
+++ Lesson_2/task5.py
@@ -0,0 +1,21 @@
+numbers=input("Enter the text: ") # input numbers (string type).
+List=list(numbers.split(", ")) # string distribution and setting the parameter.
+List1= list()
+maxNum = 0
+for number in List:
+    List1.append(int(number)) #Filling list int type numbers.
+for num1 in List1: #Searching for the biggest number in the list.
+    if maxNum<num1:
+        maxNum=num1
+print("Max number in list is: " + str(maxNum))
+List1.remove(maxNum)
+minNum = List1[0]
+for num2 in List1: #Searching for the smallest number in the list.
+    if minNum<num1:
+        maxNum=num1
+print("Min number in list is: " + str(minNum))
+List1.remove(minNum)
+sum = 0
+for num3 in List1: #Addition of the rest number in the list.
+    sum = sum + num3
+print("Sum of rest numbers is: " + str(sum))
